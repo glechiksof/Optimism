35392a1418aedfcc4f79f27b7cfb89021c2a6d16
Sofiia Khrushch <glechiksteam@gmail.com>
2026-05-16 22:54:09 +0300

    attempts to fix deploy

--- backend/tests/test_config.py
+++ backend/tests/test_config.py
@@ -14,32 +14,27 @@ def _make_settings(**kwargs):
 
 def test_cors_origins_json_array():
     s = _make_settings(CORS_ORIGINS='["https://foo.com", "https://bar.com"]')
-    assert s.CORS_ORIGINS == ["https://foo.com", "https://bar.com"]
+    assert s.cors_origins_list == ["https://foo.com", "https://bar.com"]
 
 
 def test_cors_origins_comma_separated():
     s = _make_settings(CORS_ORIGINS="https://foo.com,https://bar.com")
-    assert s.CORS_ORIGINS == ["https://foo.com", "https://bar.com"]
+    assert s.cors_origins_list == ["https://foo.com", "https://bar.com"]
 
 
 def test_cors_origins_comma_separated_with_spaces():
     s = _make_settings(CORS_ORIGINS=" https://foo.com , https://bar.com ")
-    assert s.CORS_ORIGINS == ["https://foo.com", "https://bar.com"]
+    assert s.cors_origins_list == ["https://foo.com", "https://bar.com"]
 
 
 def test_cors_origins_single_value():
     s = _make_settings(CORS_ORIGINS="https://foo.com")
-    assert s.CORS_ORIGINS == ["https://foo.com"]
+    assert s.cors_origins_list == ["https://foo.com"]
 
 
-def test_cors_origins_already_list():
-    s = _make_settings(CORS_ORIGINS=["https://foo.com"])
-    assert s.CORS_ORIGINS == ["https://foo.com"]
-
-
-def test_cors_origins_empty_string_returns_empty_list():
+def test_cors_origins_empty_string_returns_default():
     s = _make_settings(CORS_ORIGINS="")
-    assert s.CORS_ORIGINS == []
+    assert s.cors_origins_list == ["http://localhost:5173"]
 
 
 def test_jwt_secret_too_short_raises():
